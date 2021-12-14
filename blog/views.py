00d2e5da23ad4c39a1c5ef2689ed245061c6fd45
Umarjon <47208541+UmarjonRajabov@users.noreply.github.com>
2021-12-14 16:30:41 +0500

    Oxirgi o'zgarish, css fayl qo'shdim

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 # from django.http import HttpResponse
 
@@ -9,17 +9,23 @@ from django.shortcuts import render
 # def about(request):
 #     return HttpResponse('<h1>Blog About</h1>')
 
-posts =[
-    {
-        'author':'Umarjon',
-        'title':'Blog post 1',
-        'content':'Birinchi postning matni',
-        'date_posted':'Dekabr 9, 2021'
-    },
+# posts =[
+#     {
+#         'author':'Umarjon',
+#         'title':'Blog post 1',
+#         'content':'Birinchi postning matni',
+#         'date_posted':'Dekabr 9, 2021'
+#     },
+#      {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'Dekabr 11, 2021'
+#     }
 
-]
+# ]
 def home(request):
-    context={'posts':posts}
+    context={'posts':Post.objects.all()}
     return render(request, 'blog/home.html', context)
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
