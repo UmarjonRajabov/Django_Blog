9e4c9250851f52c7ce828c9ae0f28798d2509b2e
Umarjon <47208541+UmarjonRajabov@users.noreply.github.com>
2021-12-30 13:30:24 +0500

    postni o'chirish faylini qo'shdim

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 class Post(models.Model):
     title =models.CharField(max_length=100)
     content=models.TextField()
@@ -11,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
