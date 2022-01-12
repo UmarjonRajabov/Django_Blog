25b948684ed11e326d3595f690bc9a50d322eb24
Umarjon <47208541+UmarjonRajabov@users.noreply.github.com>
2022-01-12 16:31:43 +0500

    parolni almashtirish funksiyalarini qo'shish

--- django_project/settings.py
+++ django_project/settings.py
@@ -115,6 +115,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+USE_L10N = True
+
 
 
 # Static files (CSS, JavaScript, Images)
@@ -132,3 +134,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = 'umarjon.rajabov95@gmail.com'
